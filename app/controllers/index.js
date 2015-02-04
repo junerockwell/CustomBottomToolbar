396ce65825dcfc1b72ea17f383636ce7fb0ce1d4
June Rockwell <cherubeflugel@gmail.com>
2015-02-04 13:29:20 -0800

    First commit

---
+++ app/controllers/index.js
@@ -0,0 +1,17 @@
+
+$.index.open();
+
+function clickMe() {
+   /* Look at your console tab in Titanium Studio
+    * to see this message */
+   console.log("You clicked Me");
+}
+
+function popUp() {
+	alert("I'm a pop up!");
+}
+
+function extraYay() {
+   /* It just returns 3 */
+   return 1 + 2;	
+}
